bd7a9954406b9bdefb6962888025d1d5777518d0
KaylynTV39 <kaylynvannorstrand@navapbc.com>
2026-05-06 14:13:35 -0400

    fix: jump to bottom button

--- components/benefit-applications-landing.tsx
+++ components/benefit-applications-landing.tsx
@@ -18,7 +18,7 @@ const PROGRAMS = [
   { id: 'benefits-cal', name: 'BenefitsCal', website: 'https://benefitscal.com/' },
   { id: 'head-start', name: 'RCOE Early Head Start: 0-3; Head Start: 3-5', website: 'https://app.informedk12.com/link_campaigns/rcoe-head-start-ehs-application-english-electronic-form?token=RX3jMrVUfWLz3aQnjpiQpseu' },
   { id: 'nurse-family-partnership', name: 'Nurse-Family Partnership', website: 'https://forms.office.com/Pages/ResponsePage.aspx?id=yqoVt4-WGUe7BO0xcOCKaQVow3g6-R9Mh0F8VizNQzhUQ1hCNENFTFBMOVg4SElRSldIWk5BRUkxUi4u' },
-  { id: 'pregnancy-planning', name: 'RCOE Pregnancy Planning', website: 'https://secureweb.rcoe.us/ONLINEREF/' },
+  { id: 'pregnancy-planning', name: 'RCOE Referrals System', website: 'https://rrrcoe.nohosoftware.com/online_referrals/' },
 ];
 
 interface BenefitApplicationsLandingProps {
