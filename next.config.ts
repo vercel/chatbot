a16c38bbe882bacfda6a1ab677ba6a286964c41b
yanguoyu <yanguoyu@gmail.com>
2026-05-28 12:18:52 +0800

    Disable cache components by default

--- next.config.ts
+++ next.config.ts
@@ -21,7 +21,6 @@ const nextConfig: NextConfig = {
   env: {
     NEXT_PUBLIC_BASE_PATH: basePath,
   },
-  cacheComponents: true,
   devIndicators: false,
   poweredByHeader: false,
   reactCompiler: true,
@@ -44,7 +43,6 @@ const nextConfig: NextConfig = {
   },
   experimental: {
     prefetchInlining: true,
-    cachedNavigations: true,
     appNewScrollHandler: true,
     inlineCss: true,
     turbopackFileSystemCacheForDev: true,
